a78b46663b1cabf11d75fe05514e63b959d1b787
Karl <dio.lloyd@gmail.com>
2021-02-25 02:49:03 +0300

    Initial commit

---
+++ src/main/java/ru/diolloyd/webuiat/App.java
@@ -0,0 +1,13 @@
+package ru.diolloyd.webuiat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
